3f96b364016316fe27c6d0834d40714fefa46baf
Ecmel B. Canlier <me@ecmelberk.com>
2019-04-08 19:22:39 +0300

    Sidebar düzenlemeleri

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -12,108 +12,134 @@ module.exports = {
             "/",
             {
                 title: "A",
+                collapsable: false,
                 children: []
             },
             {
                 title: "B",
+                collapsable: false,
                 children: []
             },
             {
                 title: "C",
+                collapsable: false,
                 children: []
             },
             {
                 title: "D",
+                collapsable: false,
                 children: []
             },
             {
                 title: "E",
+                collapsable: false,
                 children: []
             },
             {
                 title: "F",
+                collapsable: false,
                 children: []
             },
             {
                 title: "G",
+                collapsable: false,
                 children: []
             },
             {
                 title: "H",
+                collapsable: false,
                 children: []
             },
             {
                 title: "I",
+                collapsable: false,
                 children: [
                     "/i/issizligin-kitabina-yazmak"
                 ]
             },
             {
                 title: "J",
+                collapsable: false,
                 children: []
             },
             {
                 title: "K",
+                collapsable: false,
                 children: []
             },
             {
                 title: "L",
+                collapsable: false,
                 children: []
             },
             {
                 title: "M",
+                collapsable: false,
                 children: []
             },
             {
                 title: "N",
+                collapsable: false,
                 children: []
             },
             {
                 title: "O",
+                collapsable: false,
                 children: []
             },
             {
                 title: "P",
+                collapsable: false,
                 children: []
             },
             {
                 title: "Q",
+                collapsable: false,
                 children: []
             },
             {
                 title: "R",
+                collapsable: false,
                 children: []
             },
             {
                 title: "S",
+                collapsable: false,
                 children: []
             },
             {
                 title: "T",
+                collapsable: false,
                 children: []
             },
             {
                 title: "U",
+                collapsable: false,
                 children: []
             },
             {
                 title: "V",
+                collapsable: false,
                 children: []
             },
             {
                 title: "W",
+                collapsable: false,
                 children: []
             },
             {
                 title: "X",
+                collapsable: false,
                 children: []
             },
             {
                 title: "Y",
+                collapsable: false,
                 children: []
             },
             {
                 title: "Z",
+                collapsable: false,
                 children: []
             },
         ]
