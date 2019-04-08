a978422273ed2df538f5cd7a32e1766c280988e6
Ecmel B. Canlier <me@ecmelberk.com>
2019-04-08 18:14:36 +0300

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,119 @@
+module.exports = {
+    title: "İşsizliğin Kitabı",
+    description: "İşsizliğin kitabı, A'dan Z'ye en işsizce rehberlerin bulunduğu bir kitaptır.",
+    evergreen: true,
+    themeConfig: {
+        repo: "issizler-club/kitap",
+        repoLabel: "Sen de yaz",
+        lastUpdated: "Son güncelleme",
+        editLinks: true,
+        editLinkText: "Bu sayfayı iyileştir",
+        sidebar: [
+            "/",
+            {
+                title: "A",
+                children: []
+            },
+            {
+                title: "B",
+                children: []
+            },
+            {
+                title: "C",
+                children: []
+            },
+            {
+                title: "D",
+                children: []
+            },
+            {
+                title: "E",
+                children: []
+            },
+            {
+                title: "F",
+                children: []
+            },
+            {
+                title: "G",
+                children: []
+            },
+            {
+                title: "H",
+                children: []
+            },
+            {
+                title: "I",
+                children: []
+            },
+            {
+                title: "J",
+                children: []
+            },
+            {
+                title: "K",
+                children: []
+            },
+            {
+                title: "L",
+                children: []
+            },
+            {
+                title: "M",
+                children: []
+            },
+            {
+                title: "N",
+                children: []
+            },
+            {
+                title: "O",
+                children: []
+            },
+            {
+                title: "P",
+                children: []
+            },
+            {
+                title: "Q",
+                children: []
+            },
+            {
+                title: "R",
+                children: []
+            },
+            {
+                title: "S",
+                children: []
+            },
+            {
+                title: "T",
+                children: []
+            },
+            {
+                title: "U",
+                children: []
+            },
+            {
+                title: "V",
+                children: []
+            },
+            {
+                title: "W",
+                children: []
+            },
+            {
+                title: "X",
+                children: []
+            },
+            {
+                title: "Y",
+                children: []
+            },
+            {
+                title: "Z",
+                children: []
+            },
+        ]
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+---
+sidebar: true
+editLink: false
+---
+
+# İşsizliğin Kitabı
+
+İşsizliğin kitabı, A'dan Z'ye en işsizce rehberlerin bulunduğu bir kitaptır.
+
+## Sen de yaz
+Bu kitaba, yazacak kadar işsiz olan herkesin rehberleri eklenmektedir,
+bu konu hakkında daha fazla bilgi için [GitHub](https://github.com/issizler-club/kitap)
+sayfamızı veya en kısa sürede yazılacak "İşsizler Kitabı'na yazı gönderme"
+başlığımızı ziyaret edebilirsiniz.
